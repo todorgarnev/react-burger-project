72a9ed33c1196190b0fa249100343ab573d3b16f
Todor Garnev <tgarnev@abv.bg>
2020-04-22 11:58:28 +0300

    Added axios; Added POST request for order summary.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component {
   state = {
@@ -60,7 +61,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Todor Todorakis',
+        address: {
+          street: 'Test street',
+          zipCode: 2900,
+          country: 'Bulgaristan'
+        },
+        email: 'test@test.com',
+        deliveryMethod: 'fastest'
+      }
+    };
+
+    axios.post('/orders.json', order)
+      .then(response => console.log('response >>', response))
+      .catch(error => console.log('error >>', error));
   }
 
   render() {
