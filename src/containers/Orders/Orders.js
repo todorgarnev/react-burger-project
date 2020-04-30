2b0b57080ff9840dc382d1af9c439225a27cb7f2
Todor Garnev <tgarnev@abv.bg>
2020-04-30 12:58:49 +0300

    Added  token to the requests.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import * as actions from '../../store/actions/actionCreators';
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
 
   render() {
@@ -29,11 +29,12 @@ class Orders extends Component {
 
 const mapStateToProps = state => ({
   orders: state.order.orders,
-  loading: state.order.loading
+  loading: state.order.loading,
+  token: state.auth.token
 });
 
 const mapDispatchToProps = dispatch => ({
-  onFetchOrders: () => dispatch(actions.fetchOrders())
+  onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,6 +16,18 @@ export const authFail = (error) => ({
   error: error
 });
 
+export const logout = () => ({
+  type: actionTypes.AUTH_LOGOUT
+});
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
@@ -32,6 +44,7 @@ export const auth = (email, password, isSignUp) => {
     axios.post(url, authData)
       .then(response => {
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => dispatch(authFail(err.response.data.error)));
   }
